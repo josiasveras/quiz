f49c2e1f3da0a6f5d1d2fdb9c939db8c292f2c58
Jorzias Veras <josias_veras@hotmail.com>
2025-10-16 16:39:57 -0300

    feat(test): add Unit Test for QuestionController class

--- src/test/groovy/com/app/quiz/controller/QuestionControllerSpec.groovy
+++ src/test/groovy/com/app/quiz/controller/QuestionControllerSpec.groovy
@@ -2,40 +2,51 @@ package com.app.quiz.controller
 
 import com.app.quiz.dto.QuestionResponse
 import com.app.quiz.service.QuestionService
+import org.junit.jupiter.api.BeforeEach
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(QuestionController)
 class QuestionControllerSpec extends Specification {
 
     @Autowired
-    MockMvc mockMvc
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new QuestionController(questionService))
+                .build()
+    }
 
     @MockBean
     QuestionService questionService
 
-    def "should return a question with options" () {
+    def "should return a question with options by id"() {
         given: "a mock question response"
-        def mockResponse = new QuestionResponse(
+        def mockQuestions = new QuestionResponse(
                 id: 1L,
                 text: "Qual instrumento Davi gostava de tocar?",
                 options: ["A) Tambor", "B) Harpa", "C) Flauta"]
         )
 
-        and: "the service returns the mock response"
-        questionService.getRandomQuestion() >> mockResponse
+        when: "the service returns the mock response"
+        Mockito.when(questionService.getQuestionById(ArgumentMatchers.any())).thenReturn(mockQuestions);
 
-        expect: "the endpoint responds with status 200 and valid JSON"
-        mockMvc.perform (get("/quiz/question"))
-                .andExpect (status().isOk())
-                .andExpect (jsonPath('$.text').value("Qual instrumento Davi gostava de tocar?"))
-                .andExpect (jsonPath('$.options').isArray())
-                .andExpect (jsonPath('$.options.length()').value(3))
+        then: "the endpoint responds with status 200 and valid JSON"
+        this.mockMvc.perform(get("/quiz/question/{id}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath('$.text').value("Qual instrumento Davi gostava de tocar?"))
+                .andExpect(jsonPath('$.options').isArray())
+                .andExpect(jsonPath('$.options.length()').value(3))
     }
 }
