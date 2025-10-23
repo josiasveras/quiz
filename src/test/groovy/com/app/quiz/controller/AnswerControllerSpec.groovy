6fe6c203aa8b6588f46efef9d370e75066705009
Jorzias Veras <josias_veras@hotmail.com>
2025-10-23 18:53:54 -0300

    feat: add tests for AnswerController

--- src/test/groovy/com/app/quiz/controller/AnswerControllerSpec.groovy
+++ src/test/groovy/com/app/quiz/controller/AnswerControllerSpec.groovy
@@ -14,8 +14,7 @@ import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest(AnswerController)
 class AnswerControllerSpec extends Specification {
@@ -27,78 +26,70 @@ class AnswerControllerSpec extends Specification {
     AnswerService answerService
 
     def "should validate an answer and return correct answer response"() {
-        given: "a correct mock answer request"
+        given: "a correct answer request"
         def mockAnswerRequest = new AnswerRequest(
                 questionId: 1L,
                 selectedAnswer: "B) Harpa"
         )
 
-        and: "a correct mock answer response"
+        and: "a correct answer response"
         def mockAnswerResponse = new AnswerResponse(
                 correct: true,
                 message: "Resposta correta!"
         )
 
-        and: "the service validates answer and returns the correct mock response"
+        when: "the service validates answer and returns the correct answer response"
         Mockito.when(answerService.validateAnswer(ArgumentMatchers.any())).thenReturn(mockAnswerResponse)
 
-        when: "the POST request is sent with Json body"
+        and: "the post request is sent with answer request Json body"
         def response = this.mockMvc.perform(post("/quiz/api/v1/answers").content(TestUtils.writeValueAsString(mockAnswerRequest)).contentType(MediaType.APPLICATION_JSON))
 
-        then: "the endpoint responds with status 200 and valid JSON"
+        then: "the endpoint responds with status 201 created and valid JSON"
         response.andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath('$.correct').value(true))
                 .andExpect(jsonPath('$.message').value("Resposta correta!"))
     }
 
     def "should validate an answer and return incorrect answer response"() {
-        given: "an incorrect mock answer request"
+        given: "an answer request with an incorrect answer"
         def mockAnswerRequest = new AnswerRequest(
                 questionId: 1L,
                 selectedAnswer: "C) Flauta"
         )
 
-        and: "a incorrect mock answer response"
+        and: "an incorrect answer response"
         def mockAnswerResponse = new AnswerResponse(
                 correct: false,
                 message: "Resposta incorreta."
         )
 
-        and: "the service validates answer and returns the mock response"
+        when: "the service validates answer and returns the answer response"
         Mockito.when(answerService.validateAnswer(ArgumentMatchers.any())).thenReturn(mockAnswerResponse)
 
-        when: "the POST request is sent with Json body"
+        and: "the post request is sent with Json body"
         def response = this.mockMvc.perform(post("/quiz/api/v1/answers").content(TestUtils.writeValueAsString(mockAnswerRequest)).contentType(MediaType.APPLICATION_JSON))
 
-        then: "the endpoint responds with status 200 and valid JSON"
+        then: "the endpoint responds with status 201 created and valid JSON"
         response.andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath('$.correct').value(false))
                 .andExpect(jsonPath('$.message').value("Resposta incorreta."))
     }
 
-    def "should return not found answer id response"() {
-        given: "a non-existent id in mock answer request"
+    def "should return 400 bad request error for invalid answer request"() {
+        given: "an invalid answer request"
         def mockAnswerRequest = new AnswerRequest(
-                questionId: 99L,
-                selectedAnswer: "B) Harpa"
+                selectedAnswer: ""
         )
 
-        and: "a not found mock answer response"
-        def mockAnswerResponse = new AnswerResponse(
-                correct: false,
-                message: "Pergunta não encontrada."
-        )
-
-        and: "the service validates answer and returns not found mock answer response"
-        Mockito.when(answerService.validateAnswer(ArgumentMatchers.any())).thenReturn(mockAnswerResponse)
-
-        when: "the POST request is sent with Json body with not found mock answer request"
+        when: "the post request is sent with Json body"
         def response = this.mockMvc.perform(post("/quiz/api/v1/answers").content(TestUtils.writeValueAsString(mockAnswerRequest)).contentType(MediaType.APPLICATION_JSON))
 
-        then: "the endpoint responds with status 200 and valid JSON"
-        response.andExpect(status().isCreated())
-                .andExpect(jsonPath('$.correct').value(false))
-                .andExpect(jsonPath('$.message').value("Pergunta não encontrada."))
+        then: "the endpoint responds with status 400 bad request and valid error JSON"
+        response.andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+
     }
 
 }
