b3a085cfd7b568ca0ccbca64c4eb15a4b54dc7cd
Jorzias Veras <josias_veras@hotmail.com>
2025-10-25 17:55:48 -0300

    feat: add tests for post question save new question endpoint

--- src/test/groovy/com/app/quiz/controller/QuestionControllerSpec.groovy
+++ src/test/groovy/com/app/quiz/controller/QuestionControllerSpec.groovy
@@ -1,8 +1,10 @@
 package com.app.quiz.controller
 
+import com.app.quiz.dto.QuestionRequest
 import com.app.quiz.dto.QuestionResponse
 import com.app.quiz.exception.QuestionNotFoundException
 import com.app.quiz.service.QuestionService
+import com.app.quiz.utils.TestUtils
 import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest(QuestionController)
@@ -78,4 +81,47 @@ class QuestionControllerSpec extends Specification {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
 
     }
+
+    def "should save a new question"() {
+        given: "a valid question request object"
+        def questionRequestMock = new QuestionRequest(
+                text: "Quem ensinou os discípulos a oração do 'Pai nosso'?",
+                options: ["A) Os fariseus", "B) Jesus", "C) Paulo"],
+                correctAnswer: "B) Jesus"
+        )
+
+        and: "a valid question response object"
+        def questionResponseMock = new QuestionResponse(
+                id: 37L,
+                text: "Quem ensinou os discípulos a oração do 'Pai nosso'?",
+                options: ["A) Os fariseus", "B) Jesus", "C) Paulo"]
+        )
+
+        when: "question service returns a question response"
+        Mockito.when(questionService.saveQuestion(ArgumentMatchers.any())).thenReturn(questionResponseMock)
+
+        and: "the post request is sent with valid json"
+        def response = this.mockMvc.perform(post("/quiz/api/v1/questions").content(TestUtils.writeValueAsString(questionRequestMock)).contentType(MediaType.APPLICATION_JSON))
+
+        then: "the endpoint responds with 200 ok code and valid json response"
+        response.andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+    }
+
+    def "should return 400 bad request error for invalid question request"() {
+        given: "an invalid question request"
+        def questionRequestMock = new QuestionRequest(
+                text: "",
+                options: ["", "", ""],
+                correctAnswer: ""
+        )
+
+        when: "the post request is sent with invalid question request json"
+        def response = this.mockMvc.perform(post("/quiz/api/v1/questions").content(TestUtils.writeValueAsString(questionRequestMock)).contentType(MediaType.APPLICATION_JSON))
+
+        then: "the endpoint responds with 400 bad request error"
+        response.andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+    }
+
 }
