a55a1b6f031b19b51e37741e6e2d266016a986dd
Jorzias Veras <josias_veras@hotmail.com>
2025-10-25 17:56:23 -0300

    feat: add dto question request

--- src/main/groovy/com/app/quiz/dto/QuestionRequest.groovy
+++ src/main/groovy/com/app/quiz/dto/QuestionRequest.groovy
@@ -1,5 +1,6 @@
 package com.app.quiz.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotEmpty
@@ -7,14 +8,17 @@ import jakarta.validation.constraints.Size
 
 class QuestionRequest {
 
-    @NotBlank(message = "Batata assada com carne")
+    @JsonProperty("text")
+    @NotBlank(message = "Question message must not be empty")
     String text
 
+    @JsonProperty("options")
     @NotEmpty(message = "Question options must not be empty")
     @Size(min = 3, message = "Question must contain at least 3 options values")
     @Valid
-    List<@NotBlank(message = "Options values must not be empty")  String> options = []
+    List<@NotBlank(message = "Options values must not be empty") String> options = []
 
+    @JsonProperty("correctAnswer")
     @NotBlank(message = "Text must not be null")
     String correctAnswer
 }
